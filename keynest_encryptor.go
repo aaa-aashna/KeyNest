5e966f08829dba25cbfc373346aadeabcd983a62
aaa-aashna <unityy7089@gmail.co>
2025-06-25 21:13:41 +0400

    Add Go lessons inside learn_go folder

---
+++ keynest_encryptor.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("🚀 Hello from Go! Let's build KeyNest 🔐")
+}
---
+++ learn_go/variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring variables with var keyword
+	var name string = "Ash"
+	var age int = 19
+	var is_in_clg bool = true
+	var weight float64 = 54.9
+
+	// Short variable declaration (type inferred)
+	city := "Gwalior"
+	score := 95
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("In College?:", is_in_clg)
+	fmt.Println("Weight:", weight)
+	fmt.Println("City:", city)
+	fmt.Println("Score:", score)
+
+}
